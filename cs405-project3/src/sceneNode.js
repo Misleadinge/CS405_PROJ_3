c48e07a7dea2e70cd180cc7565b6becacaeb7eae
Emre Bülbül <87551092+Misleadinge@users.noreply.github.com>
2024-01-15 22:54:32 +0300

    new sceneNode, normal was taken wrong way

--- cs405-project3/src/sceneNode.js
+++ cs405-project3/src/sceneNode.js
@@ -45,7 +45,7 @@ class SceneNode {
         //do calculkations
         var transformedMvp = MatrixMult(mvp, this.trs.getTransformationMatrix());
         var transformedModelView = MatrixMult(modelView, this.trs.getTransformationMatrix());
-        var transformedNormals = getNormalMatrix(transformedModelView);
+        var transformedNormals = MatrixMult(normalMatrix, this.trs.getTransformationMatrix());
         var transformedModel = MatrixMult(modelMatrix, this.trs.getTransformationMatrix());
 
         //draw the MeshDrawer
